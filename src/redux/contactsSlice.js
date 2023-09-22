710fd97941991938ee5eed4ea4167702862210bc
Andrii Chychkan <mywork1728@gmail.com>
2023-09-22 15:36:33 +0300

    huinia

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,36 +1,36 @@
-import { nanoid } from 'nanoid';
-import { createSlice } from '@reduxjs/toolkit';
+// import { nanoid } from 'nanoid';
+// import { createSlice } from '@reduxjs/toolkit';
 
-const contacts = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const contacts = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
-const slice = createSlice({
-  name: 'contacts',
-  initialState: contacts,
-  reducers: {
-    addContact: {
-      reducer(state, action) {
-        state.push(action.payload);
-      },
-      prepare(name, number) {
-        return {
-          payload: {
-            id: nanoid(),
-            name: name,
-            number: number,
-          },
-        };
-      },
-    },
-    deleteContact(state, action) {
-      return state.filter(el => el.id !== action.payload);
-    },
-  },
-});
+// const slice = createSlice({
+//   name: 'contacts',
+//   initialState: contacts,
+//   reducers: {
+//     addContact: {
+//       reducer(state, action) {
+//         state.push(action.payload);
+//       },
+//       prepare(name, number) {
+//         return {
+//           payload: {
+//             id: nanoid(),
+//             name: name,
+//             number: number,
+//           },
+//         };
+//       },
+//     },
+//     deleteContact(state, action) {
+//       return state.filter(el => el.id !== action.payload);
+//     },
+//   },
+// });
 
-export const { addContact, deleteContact } = slice.actions;
-export const contactsReducer = slice.reducer;
+// export const { addContact, deleteContact } = slice.actions;
+// export const contactsReducer = slice.reducer;
