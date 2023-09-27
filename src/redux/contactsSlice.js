fe28626f479de2a517452f63959481ab7e200113
Andrii Chychkan <mywork1728@gmail>
2023-09-27 23:48:13 +0300

    fuck it asldfkj;sdjf;askdfj;jkldjfasdkl

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,41 +1,7 @@
-// import { nanoid } from 'nanoid';
 import { createSlice } from '@reduxjs/toolkit';
 
-import { deleteContact, fetchContacts } from 'service/getContacts';
+import { addContact, deleteContact, fetchContacts } from 'service/getContacts';
 
-// const contacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
-
-// const slice = createSlice({
-//   name: 'contacts',
-//   initialState: contacts,
-//   reducers: {
-//     addContact: {
-//       reducer(state, action) {
-//         state.push(action.payload);
-//       },
-//       prepare(name, number) {
-//         return {
-//           payload: {
-//             id: nanoid(),
-//             name: name,
-//             number: number,
-//           },
-//         };
-//       },
-//     },
-//     deleteContact(state, action) {
-//       return state.filter(el => el.id !== action.payload);
-//     },
-//   },
-// });
-
-// export const { addContact, deleteContact } = slice.actions;
-// export const contactsReducer = slice.reducer;
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
@@ -43,23 +9,7 @@ const contactsSlice = createSlice({
     status: null,
     error: null,
   },
-  reducers: {
-    // // Виконається в момент старту HTTP-запиту
-    // fetchingInProgress(state) {
-    //   state.isLoading = true;
-    // },
-    // // Виконається якщо HTTP-запит завершився успішно
-    // fetchingSuccess(state, action) {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   state.items = action.payload;
-    // },
-    // // Виконається якщо HTTP-запит завершився з помилкою
-    // fetchingError(state, action) {
-    //   state.isLoading = false;
-    //   state.error = action.payload;
-    // },
-  },
+  reducers: {},
   extraReducers: builder => {
     builder
       .addCase(fetchContacts.pending, (state, action) => {
@@ -78,6 +28,16 @@ const contactsSlice = createSlice({
         state.contacts = state.contacts.filter(
           item => item.id !== action.payload.id
         );
+      })
+      .addCase(addContact.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        return {
+          ...state,
+          status: 'resolved',
+          contacts: [action.payload, ...state.contacts],
+        };
       });
   },
 });
